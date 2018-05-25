10e309c12af206c2c606aa9ce9ff397d3c6283aa
KimmigAlex <Kimmig.Alex@gmail.com>
2018-05-25 21:52:05 +0200

    Refactored, added comments and got main to update every 15Ms

--- Game-Of-LiveKindaWorking.asm
+++ Game-Of-LiveKindaWorking.asm
@@ -1,313 +1,243 @@
-;Countdown / Zaehler auf Null
-;in Minuten und Sekunden
+;--------------------Game of live - --------------------
 ;
-; mit 4x7-segment anzeige
-;
-; initialisierung
+; Vorlesung: 	Systemname Programmierung
+; Von:		Jennifer, Thomas und Alexander
+; https://de.wikipedia.org/wiki/Conways_Spiel_des_Lebens
+
+; -----------------
+; Startpunkt
+;------------------
 cseg at 0h
-ajmp init
+ajmp init	;Überspringe interrupts und gehe zur initialisierung
 cseg at 100h
 
 ; -----------------
-; Interrupt
+; Interrupt Timer 0
 ;------------------
-ORG 0bh
-mov tl0, #000h
-mov th0, #0fdh
-SETB TR0
-call timer
-reti
+ORG 0bh		;Einsprung Adresse für TF0
+call setTimer0	;Setzen des Startwertes von Timer0 (Da 16Bit kein automatischer reload!)
+call timer0Routine;Springe zur routine für Timer0
+reti		;Springe zur Stelle im Code vor dem Interrupt
+
+; -----------------
+; Interrupt timer 1
+;------------------
+ORG 1bh
+call setTimer1	;Setzen des Startwertes von Timer1 (Da 16Bit kein automatischer reload!)
+MOV 40,#0fh;	;Setze Speicher an Stelle 40 auf #0Fh (TODO Richtiger Merker wäre besser)
+reti		;Springe zur Stelle im Code vor dem Interrupt
+
 ;------------------
 ;init
 ;------------------
-ORG 20h
+ORG 50h
 init:
-mov IE, #10010010b
-mov tmod, #00000001b
-mov tl0, #000h
-mov th0, #0fdh
-SETB TR0
-setb P0.0
-MOV R0,#0 ;Adress for row
-;MOV R1,#20h ;Adress for row copy
+mov IE, #10001010b	;Setze EA, ET0, ET1 um die Timer einzustellen
+mov tmod, #00010001b	;Setze timer modus für T0 und T1 auf 16Bit
 
-; Initiales zeichnen des Gleiters
-MOV A,#00000000b
-movx @R0,A
+call setTimer0		;Setze Startwerte für T0
+SETB TR0		;Starte T0
+call setTimer1		;Setze Startwerte für T1
+SETB TR1		;Starte T1
 
-INC R0
-MOV A,#01111100b
-movx @R0,A
+call initCanvas
+call display
 
-INC R0
-MOV A,#10000100b
-movx @R0,A
+call main
 
-INC R0
-MOV A,#00000100b
-movx @R0,A
-
-INC R0
-MOV A,#10001000b
-movx @R0,A
-
-INC R0
-MOV A,#00100000b
-movx @R0,A
-
-INC R0
-MOV A,#00000000b
-movx @R0,A
-
-INC R0
-MOV A,#00000000b
-movx @R0,A
-
-
-
-LCALL display
-
+;------------------
+;main (loop)
+;------------------
 main:
-call new_generation
-jmp main
+MOV A,40		;Lade byte welches von T1 gesetzt wird in Akku
+JZ main			;Wenn es null versuche es nochmal
+MOV 40,#0		;Sonst setze es auf null...
+call NEW_GENERATION	;...und berechne die neue Generation
+jmp main		;Danach wieder von vorne
 
-;---------------------------
-anfang:
-jnb p1.0, starttimer
-nurRT:
-jnb p1.2, RT
-jnb tr0, da
-ajmp anfang
-da:
-lcall display
-jnb P0.0, nurRT
-ajmp anfang
-;------------------------------
-; unterprogramme
-;
-; start timer
-starttimer:
-setb tr0; start timer0
-setb P1.0
-ajmp anfang
-; stop Timer
-;stoptimer:
-;clr tr0; stop timer
-;setb P1.1
-;ajmp anfang
+;------------------
+;Rücksetzten der Timer
+;------------------
+setTimer0:
+mov tl0, #000h		;Setzen des niederwertigen start bytes von T0
+mov th0, #0fdh		;Setzen des höherwertigen start bytes von T0
+SETB TR0		;Starte Timer 0
+ret
 
-RT:
-clr tr0; stop timer
-setb P1.2
-setb P0.0
-ljmp init
-
-jmpdisplay:
-lcall display
+setTimer1:
+mov tl1, #068h		;Setzen des niederwertigen start bytes von T1
+mov th1, #0C5h		;Setzen des höherwertigen start bytes von T1
+SETB TR1		;Starte Timer 1
 ret
 
 ;----------------------
-; timer
-
-;
-timer:
-push 00h
-push A
-call display
-pop A
-pop 00h
+; timer0 inerrupt routine
+;----------------------
+timer0Routine:
+push 00h		;Speichern von R0 auf dem Stack, damit dieser nach der Interrupt routine wieder zurück gesetzt werden kann
+push A			;Das gleiche für den Akku
+call display		;Einmal das Spielfeld zeichnen
+pop A			;Rücksetzten von A aus dem Stack
+pop 00h			;Das gleiche für R0
 ret
 
-;hupe:
-;clr tr0; stop timer
-;clr P0.0
-;ret
-
-;----- Logik für die neue Generation -----
+;----------------------
+; Logik für die neue Generation
+;----------------------
 new_generation:
-MOV R0,#0;
-MOV R2,#0 ;Counter row starts with row 0
+MOV R2,#0		;Zeilen Zähler (0-7)
 
-checkrow:
-MOV R3,#0 			;Counter column starts with column 0
-MOV R4,#0			;R4 enthält die neuen Werte der Zellen der Zeile
-MOV R1,#0;
+checkrow:		;Iteriert durch alle Zeilen
+MOV R3,#0 		;Spalten Zähler (0-7)
+MOV R4,#0		;R4 ist ein Zwischenspeicher für die neue Zeile
+MOV R1,#0		;TODO
 
-checkcolumn:
-MOV R7,#0
-mov DPTR,#table2		;R7 enthält die anzahl der nachbarn
-ACALL checkTop
-ACALL checkMid
-ACALL checkBottom
+checkcolumn:		;Iteriert durch alle Spalten
+MOV R7,#0		;Zurücksetzen von R7 (Speichert die anzahl an Nachbarn einer Zelle zwischen)
+mov DPTR,#table2	;Laden der Masken für die Nachbar-Checks
+ACALL checkTop		;Überprüfen wie viele Nachbarn in der Zeile oberhalb der Zelle sind
+ACALL checkMid		;Überprüfen wie viele Nachbarn in der Zeile der Zelle sind
+ACALL checkBottom	;Überprüfen wie viele Nachbarn in der Zeile unterhalb der Zelle sind
 
-mov DPTR,#table			;Setze DataPointer auf masken tabelle
-mov A,R3
-movC A,@A+DPTR			;Hole Maske für Spalte R3
-mov R6,A;
+mov DPTR,#table		;Setze DataPointer auf masken tabelle für das hinzufügen der neuen Zelle
+mov A,R3		;Lade die aktuelle Spaltennummer
+movC A,@A+DPTR		;Hole Maske dass nur die Spalte gesetzt wird (z.B. Maske Spalte 0: #10000000b, Maske Spalte 5: #00000100b, usw.)
+mov R6,A;		;Zwischenspeichern des Maske in R6
 
-MOV A,R2;
+MOV A,R2;		;Kopiere aktuelle Zeile der Zelle in R0
 MOV R0,A;
-MOVX A,@R0;			;Laden der entsprechenden Zeile
-MOV R5,A;
-;MOV R1,#0
+MOVX A,@R0;		;Lade Zeileninhalt aus Speicher
+MOV R5,A;		;Zwischenspeichern der Zeile in R5
 
-MOV A,R7;
-CJNE A,#2,checkLessThan2	;Wenn A != 2 überprüfe ob weniger
-MOV A,R6			;Wenn A = 2 -> Behalte Wert der Zelle bei. Hole Maske
-ANL A,R5			;Maskiere Wert der Zelle aus
-ORL A,R1			;Berechne neue Zeile
-MOV R1,A			;Füge Wert zur neuen Zeile hinzu
-JMP checkEnd
+MOV A,R7;		;Hole die Anzahl an Nachbarn in den Akku zum vergleichen
+CJNE A,#2,checkUnderPopulation;Überprüfe ob es zwei Nachbarn sind, wenn nicht dann ob mehr oder weniger
+MOV A,R6		;Wenn A = 2 -> Behalte Wert der Zelle bei und lade die in R6 zwischengespeicherte Maske
+ANL A,R5		;Maskiere Wert der Zelle aus
+ORL A,R1		;Und füge ihn zur neuen Zeile hinzu
+MOV R1,A		;Speichere den Wert der aktualisierten, neuen Zeile zwischen
+JMP checkEnd		;Die Überprüfung der Zelle ist zu Ende
 
-checkLessThan2:
-JNC checkMoreThan2		;Wenn A < 2 -> C = 1
-JMP checkEnd
+checkUnderPopulation:	;Überprüfe ob eine Unterpopulation vorliegt und die Zelle an Einsamkeit stirbt
+JNC checkPerfectPopulation;Wenn A > 2, dann wurde das Carry bit durch CJNE auf 0 gesetzt -> Sprung zu checkPerfectPopulation
+JMP checkEnd		;Sonst war es eine Unterpopulation und die Zelle stirbt (Wird nicht gesetzt) und die Überprüfung endet
 
-checkMoreThan2:
-CJNE A,#3,checkEnd		;Wenn A != 3 muss die Zelle sterben
-MOV A,R6			;Wenn A = 3 -> Setze Bit
-ANL A,#11111111b		;Maskiere Wert der Zelle aus
-ORL A,R1			;Berechne neue Zeile
-MOV R1,A			;Füge Wert zur neuen Zeile hinzu
-JMP checkEnd
+checkPerfectPopulation: ;Überprüfe ob es genau 3 Nachbarn gibt und die Zelle reanimiert wird
+CJNE A,#3,checkEnd	;Wenn A != 3 können es jetzt nur noch mehr sein -> Zelle stirbt (wird nicht gesetzt)
+MOV A,R6		;Lade die in R6 gespeicherte Maske
+ANL A,#11111111b        ;Setze die Zelle durch die Maske an der entsprechenden Stelle auf 1
+ORL A,R1		;Berechne neue Zeile
+MOV R1,A		;Füge Wert zur neuen Zeile hinzu
+JMP checkEnd		;Die Überprüfung endet hier
 
-checkEnd:
-;A < 2 CLR
-;A = 3 SETB
-;A > 3 CLR
-
-;Repeat until column = 0
-INC R3
-MOV A,R3
-CJNE A,#8,checkcolumn
-
- ;setting new row from storage copy
-;movx @R0,TODO
-
-;Repeat until row = 0
-MOV A,R2;
-ADD A,#100;
-MOV R0,A;
-MOV A,R1;
-MOVX @R0,A;
-
-INC R2
-MOV A,R2
-CJNE A,#8,checkrow
-
-call copyNewGenerationToField
-ret
+checkEnd:		;Sprungpunkt wenn die Überprüfungen enden
+INC R3			;Die Nächste Spalte wird betrachtet
+MOV A,R3		;Lade Spalten Nr. in Akku zum Vergleichen
+CJNE A,#8,checkcolumn	;Wenn die Spalten Nr. 8 entspricht wurde die komplette Zeile bearbeitet und es findet KEIN Sprung zu "checkcolumn" statt
 
 
+MOV A,R2;		;Stattdessen wird der Zeilenzähler zum Vergleichen in den Akku geladen
+ADD A,#100;		;Und es wird 100 dazu addiert (Um sicherzugehen dass keine schon belegte Speicherstelle adressiert wird)
+MOV R0,A;		;Die Speicherstellen Adresse wird in R0 geschrieben
+MOV A,R1;		;Und der zu speichernde Inhalt (die neue Zeile) in den Akku kopiert
+MOVX @R0,A;		;Anschließend wird die Zeile in den Speicher geschreiben
+
+INC R2			;Die Zeilen Nr. um ein erhöht
+MOV A,R2		;und zu Vergleichszwecken un den Akku geschrieben
+CJNE A,#8,checkrow	;Wenn die Zeilen Nr. 8 entspricht wurden alle Zeilen bearbeitet und die neue Generation ist fertig. Es findet KEIN Sprung zu "checkrow" statt
+
+call copyNewGenerationToField;Die neue Generation wird nun vom Zwischenspeicher auf das richtige Feld geladen
+ret			;Fertig, kehre zur Hauptschleife "main" zurück
+
+;----------------------
+; Logik zum ermitteln der Anzahl an Nachbarn in der selben Zeile, der oberhalb und der unterhalb der Zelle
+;----------------------
 checkTop:
-;Lade Zähler der Zeile in Akku
-MOV A,R2;
-;Wenn Akku gleich 0 muss zeile 7 betrachtet werden
-MOV R5,#7;
-JZ gotTopRow
-;Sonst wird die Aktuelle zeile gesetzt
-DEC A;
-MOV R5,A;
-;gotRow dient dazu, dass falls R2 = 0 R0 = 7 bleibt
+MOV A,R2		;Lade Zeilen Nr.
+MOV R5,#7;		;Wenn Zeilen Nr. = 0 ist die unterstee Zeile die "darüber", da das Spielfeld sphärisch ist
+JZ gotTopRow		;Falls Zeilen Nr. = 0 springe zu "gotTopRow" da keine weitere berechnung nötig
+DEC A;			;Falls nicht entspricht die Zeilen Nr. darüber der Nr. im Akku - 1
+MOV R5,A;		;Speichern der Zeilen Nr. in R5
+
 gotTopRow:
-ACALL calculateNeighbours
-ret
+CALL calculateNeighbours;Berechne die Nachbarn für die Zeile
+ret			;Fertig
 
 checkMid:
-;Lade Zähler der Zeile in Akku
-MOV A,R2;
-MOV R5,A;
-ACALL calculateMiddleNeighbours
-;DEC R7; ;NUR WENN MITTE GESEETZT
+MOV A,R2;		;Lade Zeilen Nr.
+MOV R5,A;		;Speicher Zeilen Nr. in R5 zwischen
+ACALL calculateMiddleNeighbours;Berechne die Nachbarn für die Mittlere Zeile (Eigene Zelle wird nicht addiert!)
 ret
 
 checkBottom:
-;Lade Zähler der Zeile in Akku
-MOV A,R2;
-;Wenn Akku gleich 7 muss zeile 0 betrachtet werden
-MOV R5,#0;
+MOV A,R2;		;Lade Zeilen Nr.
+MOV R5,#0;		;Wenn Zeilen Nr. = 7 ist die oberste Zeile die darunter, da das Spielfeld sphärisch ist
 CJNE A,#7,getBottomRow
-JMP gotBottomrow
-;Sonst wird die Aktuelle zeile gesetzt
+JMP gotBottomrow	;Wenn die Zeilen Nr. = 7 muss nichts mehr getan werden
 getBottomRow:
-INC A;
-MOV R5,A;
-;gotRow dient dazu, dass falls R2 = 7 R0 = 0 bleibt
+INC A;			;Wenn sie != 7 dann ist die untere Zeilen Nr. A - 1
+MOV R5,A;		;Speichern der Unteren Zeilen Nr. in R5
 gotBottomRow:
-ACALL calculateNeighbours
+CALL calculateNeighbours;Berechne die Nachbarn für die Zeile
 ret
 
 calculateNeighbours:
-MOV A,R5;
-MOV R0,A;
+MOV A,R5		;Laden der Zeilen Nr.
+MOV R0,A
 
-MOVX A,@R0;	;Laden der entsprechenden Zeile
-MOV R6,A	;Speichern der rotierten Zeile
+MOVX A,@R0;		;Laden der entsprechenden Zeile
+MOV R6,A		;Speichern der rotierten Zeile
 
-MOV A,R3		;7-ZeilenNummer den benötigten rechts rotationen entspricht
-MOVC A,@A+DPTR
-ANL A,R6;
-JZ neighbour1NotSet
-INC R7;
+call checkLeftNeighbour;Ermitteln ob linker Nachbar gesetzt ist
+call checkMiddleNeighbour;Ermitteln ob mittlerer Nachbar gesetzt ist
+call checkRightNeighbour;Ermitteln ob rechter Nachbar gesetzt ist
 
-neighbour1NotSet:
-MOV A,R3		;7-ZeilenNummer den benötigten rechts rotationen entspricht
-INC A
-MOVC A,@A+DPTR
-ANL A,R6;
-JZ neighbour2NotSet
-INC R7;
-
-neighbour2NotSet:
-MOV A,R3		;7-ZeilenNummer den benötigten rechts rotationen entspricht
-INC A
-INC A
-MOVC A,@A+DPTR
-ANL A,R6;
-JZ neighbour3NotSet
-INC R7;
-
-neighbour3NotSet:
-MOV R6,#0;
+MOV R6,#0		;Zurücksetzten von R6
 ret
-
 
 calculateMiddleNeighbours:
 MOV A,R5;
 MOV R0,A;
-MOVX A,@R0;	;Laden der entsprechenden Zeile
-MOV R6,A	;Speichern der rotierten Zeile
-;Berechnen um wie viele Stellen die Zeile nach rechts verschoben werden muss
+MOVX A,@R0;		;Laden der entsprechenden Zeile
+MOV R6,A		;Speichern der rotierten Zeile
 
-;MOV R4,A	;Speichern des zählers
+call checkLeftNeighbour;Ermitteln ob linker Nachbar gesetzt ist
+call checkRightNeighbour;Ermitteln ob rechter Nachbar gesetzt ist
 
-
-;Rotieren der Zeile bis der zähler R4 null ist
-;RL A		;einmalig links rotieren da erst nach der rechts rotation auf 0 geprüft wird
-;RL A		;einmalig links rotieren da erst nach der rechts rotation auf 0 geprüft wird
-;rotateRight2:	;so wird verhindert dass wenn der zähler 0 ist eine rotation stattfindet
-;RR A;
-;DJNZ R4,rotateRight2
-
-MOV A,R3		;7-ZeilenNummer den benötigten rechts rotationen entspricht
-MOVC A,@A+DPTR
-ANL A,R6;
-JZ neighbour2NotSet2
-INC R7;
-
-neighbour2NotSet2:
-MOV A,R3		;7-ZeilenNummer den benötigten rechts rotationen entspricht
-INC A
-INC A
-MOVC A,@A+DPTR
-ANL A,R6;
-JZ neighbour3NotSet2
-INC R7;
-
-neighbour3NotSet2:
-MOV R6,#0;
+MOV R6,#0		;Zurücksetzten von R6
 ret
 
+
+;----------------------
+; Logik zum ermitteln der Nachbarn Rechts, Links und in der selben Spalte einer Zelle
+;----------------------
+checkLeftNeighbour:
+MOV A,R3		;Lade Spalten Nr. in R3
+call incIfSet		;Wenn die Spalte A der Zeile gesetzt ist wird R7 erhöht
+ret			;Fertig -> Rücksprung
+
+checkMiddleNeighbour:
+MOV A,R3		;Lade Spalten Nr. in R3
+INC A			;Erhöhe Spalten Nr. um eins um den mittleren Nachbarn auszumaskieren
+call incIfSet		;Wenn die Spalte A der Zeile gesetzt ist wird R7 erhöht
+ret			;Fertig -> Rücksprung
+
+checkRightNeighbour:
+MOV A,R3		;Lade Spalten Nr. in R3
+ADD A,#2		;Erhöhe Spalten Nr. um zwei um den rechten Nachbarn auszumaskieren
+call incIfSet		;Wenn die Spalte A der Zeile gesetzt ist wird R7 erhöht
+ret			;Fertig -> Rücksprung
+
+incIfSet:
+MOVC A,@A+DPTR		;Lade Maske für die Spalte A
+ANL A,R6;		;Ermittel den Zustand der Spalte mithilfe der Zeile R6
+JZ rightCheckDone	;Wenn A = 0 war die Zelle nicht gesezt und das erhöhen des Nachbar Zählers wird übersprungen
+INC R7;			;Sonst: Erhöhen des Nachbarzählers
+rightCheckDone:
+ret			;Fertig -> Rücksprung
+
+;----------------------
+; Kopieren der neuen Generation auf die Speicherstellen welche welche gezeichnet werden
+;----------------------
 copyNewGenerationToField:
 MOV R0,#100
 MOV R1,#0
@@ -351,6 +281,9 @@ MOVX @R1,A
 
 ret
 
+;----------------------
+; Zeichen der Speicher stelle 0h bis 7h auf die LED-Matrix
+;----------------------
 display:
 mov 42,A
 
@@ -406,7 +339,46 @@ mov A,42
 
 ret
 
+; -----------------
+; Initialisieren der Zeichenfläche
+;------------------
+initCanvas:
+Mov R0,#0;
+MOV A,#00000000b	;Zeile 0
+movx @R0,A		;Speichern
 
+INC R0
+MOV A,#01111100b
+movx @R0,A
+
+INC R0
+MOV A,#10000100b
+movx @R0,A
+
+INC R0
+MOV A,#00000100b
+movx @R0,A
+
+INC R0
+MOV A,#10001000b
+movx @R0,A
+
+INC R0
+MOV A,#00100000b
+movx @R0,A
+
+INC R0
+MOV A,#00000000b
+movx @R0,A
+
+INC R0
+MOV A,#00000000b
+movx @R0,A
+ret
+
+; -----------------
+; Masken, sollten noch kombiniert werden ;)
+;------------------
 table: db 10000000b, 01000000b, 00100000b, 00010000b, 00001000b, 00000100b, 00000010b, 00000001b
 table2: db 00000001b, 10000000b, 01000000b, 00100000b, 00010000b, 00001000b, 00000100b, 00000010b, 00000001b, 10000000b
 
